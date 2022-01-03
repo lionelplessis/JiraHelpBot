a294aef7849a976f72f7e765ceef49e8729fdb74
Lionel Plessis <lionel_plessis@yahoo.fr>
2022-01-03 15:17:03 +0100

    Move MS Teams app package resources/manifest and add 1 for JiraHelpBot2

---
+++ Src/JiraHelpBot/JiraClient/Client/IJiraService.cs
@@ -0,0 +1,12 @@
+﻿#region copyright
+// Copyright 2007 - 2021 Innoveo AG, Zurich/Switzerland
+// All rights reserved. Use is subject to license terms.
+#endregion
+
+namespace JiraHelpBot.JiraClient.Client
+{
+    public interface IJiraService
+    {
+        
+    }
+}
